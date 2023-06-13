040ac32b47ac3040edcb7c061c5aaff99f0fea9e
Prathamj20 <prathamj20@gmail.com>
2023-06-13 22:37:56 +0530

    Added query for order count per customer

---
+++ SQL-Queries/1. total_orders_by_customer.sql
@@ -0,0 +1,7 @@
+
+/*
+The query SELECT COUNT(order_id) FROM orders; retrieves the count of the order_id column from the orders table.
+In other words, it calculates the total number of rows or records in the orders table. The result of this query will be a single value representing the count of order IDs present in the table.
+*/
+
+SELECT COUNT(order_id) FROM orders;
