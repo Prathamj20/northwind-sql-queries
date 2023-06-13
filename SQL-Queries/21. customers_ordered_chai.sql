09e2a44b2141db3da10a11840a263aa042b3d887
Prathamj20 <prathamj20@gmail.com>
2023-06-13 22:47:19 +0530

    Added query for 'Chai' product customers

---
+++ SQL-Queries/21. customers_ordered_chai.sql
@@ -0,0 +1,9 @@
+/*
+This query retrieves the customer IDs and contact names of customers who have placed orders for a product named 'Chai'. It combines information from the "customers," "orders," "order_details," and "products" tables using JOIN statements. The WHERE clause filters the results to include only the rows where the product name is 'Chai'. In summary, the query identifies customers who have ordered the 'Chai' product and retrieves their customer IDs and contact names.
+*/
+SELECT customers.customer_id, customers.contact_name
+FROM customers
+JOIN orders ON customers.customer_id = orders.customer_id
+JOIN order_details ON orders.order_id = order_details.order_id
+JOIN products ON order_details.product_id = products.product_id
+WHERE products.product_name = 'Chai';
