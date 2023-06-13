0ab6bf73ccd1d871dcc5841d2813e67abef73c00
Prathamj20 <prathamj20@gmail.com>
2023-06-13 22:45:54 +0530

    Added query for distinct shipping countries

---
+++ SQL-Queries/17. distinct_shipped_countries.sql
@@ -0,0 +1,5 @@
+/*
+This query retrieves a list of unique ship countries from the "orders" table. In simpler terms, it shows the distinct or unique countries where orders have been shipped to.
+*/
+SELECT DISTINCT ship_country
+FROM orders;
