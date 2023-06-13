4ec7af454e592b005b7fef74060242e3fd88f3ca
Prathamj20 <prathamj20@gmail.com>
2023-06-13 22:48:17 +0530

    Added query for customer with most 'Chai' orders

---
+++ SQL-Queries/27. average_quantity_per_order.sql
@@ -0,0 +1,6 @@
+/* This query retrieves the order ID and the average quantity of items for each order from the "order_details" table. It calculates the average quantity by grouping the records based on the order ID. The result is a summary that shows the average quantity for each order.*/
+
+SELECT order_id, AVG( quantity) AS AverageQuantity
+FROM order_details
+GROUP BY order_id;
+
