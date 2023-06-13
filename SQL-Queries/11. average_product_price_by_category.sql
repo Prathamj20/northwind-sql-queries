b23a15ac8d32f8dcab237dd56a508696d9b45a0b
Prathamj20 <prathamj20@gmail.com>
2023-06-13 22:44:45 +0530

    Added query for average product price

---
+++ SQL-Queries/11. average_product_price_by_category.sql
@@ -0,0 +1,7 @@
+/*
+This query calculates the average price of products within each category. It retrieves the category name and the corresponding average price. The query achieves this by joining the "products" and "categories" tables based on their respective IDs. It then groups the results by the category name to provide the average price for each category. In simpler terms, the query gives you the average price of products for each product category.
+*/
+SELECT categories.category_name, AVG(products.unit_price) AS AveragePrice
+FROM products
+JOIN categories ON products.category_id = categories.category_id
+GROUP BY categories.category_name;
